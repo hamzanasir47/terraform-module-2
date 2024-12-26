096c2c18bcedd333f67ededcbbd2ea272f5d66ac
Hamza Nasir <101545759+hamzanasir47@users.noreply.github.com>
2024-12-26 15:57:43 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "9dfcb940-4a7b-4e3f-b55d-e9fd0dece2eb"
+  #subscription_id = "9dfcb940-4a7b-4e3f-b55d-e9fd0dece2eb"
   features {}
   #version = "3.0.0"
   #use_azure_cli = true
